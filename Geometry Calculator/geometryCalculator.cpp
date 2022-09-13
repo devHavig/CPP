448bb86055bd716496cf7eaa186fce407e5c04fd
JerrodxHavig <jhavig99@gmail.com>
2022-09-13 14:57:35 -0700

    program logic

--- Geometry Calculator/geometryCalculator.cpp
+++ Geometry Calculator/geometryCalculator.cpp
@@ -10,12 +10,32 @@ int main()
 
     cout << endl;
     cout << "Geometry Calculator" << endl;
-    cout << "1. Calulate the Area of a  Circle" << endl; 
-    cout << "2. Calulate the Area of a Rectangle" << endl; 
-    cout << "3. the Area of a Triangle" << endl; 
-    cout << "4. Quit" << endl; 
+    cout << "1. Calulate the Area of a  Circle" << endl;
+    cout << "2. Calulate the Area of a Rectangle" << endl;
+    cout << "3. the Area of a Triangle" << endl;
+    cout << "4. Quit" << endl;
     cout << endl;
     cout << "Enter your choice (1-4):" << endl;
+
+    switch (user_choice)
+    {
+    case 1:
+        float radius;
+        float area = PI * pow(radius, 2);
+        break;
+    case 2:
+        float length;
+        float width;
+        area = length * width;
+        break;
+    case 3:
+        float base;
+        float height;
+        area = base * height * .5;
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
