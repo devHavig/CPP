7e89999a9325972eae6fdbf49abbe9c16540703e
JerrodxHavig <jhavig99@gmail.com>
2022-09-13 15:55:40 -0700

    choices  1-2 finished

--- Geometry Calculator/geometryCalculator.cpp
+++ Geometry Calculator/geometryCalculator.cpp
@@ -16,22 +16,68 @@ int main()
     cout << "4. Quit" << endl;
     cout << endl;
     cout << "Enter your choice (1-4):" << endl;
+    cin >> user_choice;
+    cout << endl;
 
     switch (user_choice)
     {
+        float area;
     case 1:
-        float radius;
-        float area = PI * pow(radius, 2);
+        int radius;
+
+        cout << "Enter  the radius: ";
+        cin >> radius;
+
+        if (radius < 0)
+        {
+            cout << "The radius must be a positive number";
+            cout << "Try again" << endl;
+        }
+        else
+        {
+            area = PI * pow(radius, 2);
+
+            cout << "The area of a circle is: " << endl;
+            cout << area << endl;
+        }
         break;
     case 2:
-        float length;
-        float width;
-        area = length * width;
+        float length, width;
+
+        if (radius < 0)
+        {
+            cout << "The radius must be a positive number";
+            cout << "Try again" << endl;
+        }
+        else
+        {
+            cout << "What is the length?" << endl;
+            cin >> length;
+            cout << "What is the width?" << endl;
+            cin >> width;
+            cout << endl;
+
+            area = length * width;
+            
+            cout << "The Area of a Rectangle is: " << endl;
+            cout << area << endl;
+        }
         break;
     case 3:
-        float base;
-        float height;
-        area = base * height * .5;
+        float base, height;
+
+        if (radius < 0)
+        {
+            cout << "The radius must be a positive number";
+            cout << "Try again" << endl;
+        }
+        else
+        {
+            area = base * height * .5;
+
+            cout << "The area of a triangle is: " << endl;
+            cout << area << endl;
+        }
         break;
     default:
         break;
