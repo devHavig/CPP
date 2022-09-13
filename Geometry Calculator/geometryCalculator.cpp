05d77297c2d8e3ec1259927faeb65495fa778bb6
JerrodxHavig <jhavig99@gmail.com>
2022-09-13 14:48:17 -0700

    created cout table

--- Geometry Calculator/geometryCalculator.cpp
+++ Geometry Calculator/geometryCalculator.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    
+    const float PI = 3.14159;
+    int user_choice;
+
+    cout << endl;
+    cout << "Geometry Calculator" << endl;
+    cout << "1. Calulate the Area of a  Circle" << endl; 
+    cout << "2. Calulate the Area of a Rectangle" << endl; 
+    cout << "3. the Area of a Triangle" << endl; 
+    cout << "4. Quit" << endl; 
+    cout << endl;
+    cout << "Enter your choice (1-4):" << endl;
+
     return 0;
 }
